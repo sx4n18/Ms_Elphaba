b1813a6405e9a41410a8a359cfd8b62b72df6d73
Shouyu Xie <shouyu.xie@manchester.ac.uk>
2025-05-01 17:10:57 +0100

    updated the encotropy encoding with rANS class

--- Enc/Entropy_coding.py
+++ Enc/Entropy_coding.py
@@ -61,6 +61,81 @@ class LZ77:
 
 
 class rAsymmetric_Numeral_System:
+    '''
+    A simple range asymmetric numeral system for encoding and decoding.
+    This encoder will encode the input message into a big integer and number of messages it encodes.
+
+    To instantiate the class, you need to provide the following parameters:
+    - labelling: a string/list of symbols randomly interleaved, where the appearance counts of each symbol
+    corresponds to its occurring probability.
+    - block_size: the size of the block
+    '''
+    def __init__(self, labelling: str | list):
+        self.labelling = labelling
+        self.block_size = len(labelling)
+
+        # `symbol` appears in each block `count_per_block[symbol]` times
+        # `count_before_index[i]` is the number of numbers labeled `labeling[i]` that are less than `i`
+        # `symbol_table[symbol][i]` is the index in `labeling` of the `i`th number labeled `symbol`
+        self.count_per_block = {}
+        self.count_before_index = []
+        self.symbol_table = {}
+        for i, c in enumerate(labelling):
+            if c not in self.symbol_table:
+                self.count_per_block[c] = 0
+                self.symbol_table[c] = []
+            self.count_before_index.append(self.count_per_block[c])
+            self.count_per_block[c] += 1
+            self.symbol_table[c].append(i)
+
+
+        # if the initial value of `X` is too small, then `C(X)` might equal `X`. but we want `C(X)` to be different from `X` for everything to be reversible
+        # this is analogous to "appending a non-zero digit to the left of `X`" except in ANS it's "non-first-symbol symbol" instead
+        # if you're sure that no message will ever start with the first symbol, you can set `initial_state = 0`
+        self.initial_state = next((i for i, c in enumerate(labelling) if c != labelling[0]), len(labelling))
+
+    def Cr(self, state: int, symbol: str):
+        """Returns the `state + 1`th number labeled `symbol`"""
+
+        # full_blocks * count_per_block[symbol] + symbols_left = state + 1
+        full_blocks = (state + 1) // self.count_per_block[symbol]
+        symbols_left = (state + 1) % self.count_per_block[symbol]  # equivalently, (state + 1) - full_blocks * self.count_per_block[symbol]
+        if symbols_left == 0:
+            full_blocks -= 1
+            symbols_left = self.count_per_block[symbol]
+
+        # Count `symbols_left` symbols within the block
+        index_within_block = self.symbol_table[symbol][symbols_left - 1]
+
+        # if `block_size` is a power of 2, this multiplication is a bitshift
+        return full_blocks * self.block_size + index_within_block
+
+
+    def Dx(self, state: int):
+        """Counts the number of numbers labeled `symbol` that are less than `state`"""
+
+        index_within_block = state % self.block_size  # if `block_size` is a power of 2, this is `state & (block_size - 1)`
+        symbol = self.labelling[index_within_block]
+
+        num_previous_blocks = state // self.block_size  # if `block_size` is a power of 2, this division is a bitshift
+        count_before_block = self.count_per_block[symbol] * num_previous_blocks
+
+        return symbol, count_before_block + self.count_before_index[index_within_block]
+
+
+    def encode(self, message: str | list):
+        state = self.initial_state
+        for symbol in message[::-1]:
+            state = self.Cr(state, symbol)
+        return state
+
+    def decode(self, state: int):
+        message = ""
+        while state > self.initial_state:
+            symbol, state = self.Dx(state)
+            message += symbol
+        return message, state
+
 
 
 if __name__ == "__main__":
